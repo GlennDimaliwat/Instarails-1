cda29dd1414a0d0d7fe5043d7bc49b2455619d18
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-09-27 14:31:24 +1000

    Add feature to comment on photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  root 'feed#index'
   resources :photos do
+    resources :comments # /photos/:photo_id/comments
     member do
       resource :photo_like, path: 'like', only: [:update]
     end
     # PATCH /photos/:id/like
-
-    #resources :comments
   end
   devise_for :users
   resources :users, only: [:index, :show]
-  root 'feed#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927003507) do
+ActiveRecord::Schema.define(version: 20170927015909) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_comments_on_created_at"
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", id: false, force: :cascade do |t|
-    t.integer  "photo_id",   null: false
-    t.integer  "user_id",    null: false
+    t.integer "photo_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at"
     t.index ["photo_id", "user_id"], name: "index_likes_on_photo_id_and_user_id", unique: true
   end
 
   create_table "photo_likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "photo_id"
+    t.integer "user_id"
+    t.integer "photo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["photo_id"], name: "index_photo_likes_on_photo_id"
@@ -30,27 +41,27 @@ ActiveRecord::Schema.define(version: 20170927003507) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "image"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "user_id"
+    t.string "image"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
