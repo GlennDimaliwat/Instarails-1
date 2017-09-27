418a189fc4a05207b1a36365889d654f2bdd7b3e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-09-27 11:53:44 +1000

    Add feature to like photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,8 +3,24 @@ class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :likes, class_name: 'PhotoLike'
 
-  def liked_by?(user)
-    PhotoLike.where(photo: self, user: user).exists?
-    #likes.where(user: user).count > 0
-  end
+  # def liked_by?(user)
+  #   PhotoLike.where(photo: self, user: user).exists?
+  #   #likes.where(user: user).count > 0
+  # end
+  has_and_belongs_to_many :likers, class_name: 'User', join_table: :likes
+  
+    def liked_by?(user)
+      likers.exists?(user.id)
+    end
+  
+    def toggle_liked_by(user)
+      # If photo has been liked by 'user'
+      if likers.exists?(user.id)
+        likers.destroy(user.id)
+  
+      # If photo has *not* been liked by the 'user'
+      else
+        likers << user
+      end
+    end
 end
